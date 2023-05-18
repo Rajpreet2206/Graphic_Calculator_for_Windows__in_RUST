c23792f2b635c67189ed69912236a139efa94ed5
Rajpreet2206 <rajpreet2206@gmail.com>
2023-05-19 04:49:49 +0530

    rect in cpp_ Basic class design

--- geometries.cpp
+++ geometries.cpp
@@ -1,1 +1,16 @@
+class Rectangle{
+    protected:
+        int width;
+        int height;
+    
+    public:
+        Rectangle(int width, int height) : width(width), height(height){}
+        int get_area(){return width*height;}
+        int get_width(){return height;}
+};
 
+class Square: public Rectangle{
+    public:
+        Square(int length) : Rectangle(length, length){}
+        int get_length(){return width;}
+};
